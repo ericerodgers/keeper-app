b0ec8c67f0f9bb6760c41fa9b4a4515a46f15911
ericerodgers <ericerodgers@yahoo.com>
2020-12-19 21:31:58 -0500

    part 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../src/Footer';
+import Header from '../src/Header';
+import Note from '../src/Note';
+
+const App = () => {
+    return (
+    <>
+    <Header />
+    <Note />
+    <Footer />
+    </>
+    )
+}
+
+export default App
